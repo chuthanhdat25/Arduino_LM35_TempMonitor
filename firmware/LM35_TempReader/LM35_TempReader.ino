7526ff174f3274ceb4040ac528b97ed1cd5b199c
macOS <macos@macOSs-MBP.fpt>
2026-04-14 15:02:46 +0700

    feat: hoan thien cau truc thu muc du an (firmware, pc_app, simulation)

--- firmware/LM35_TempReader/LM35_TempReader.ino
+++ firmware/LM35_TempReader/LM35_TempReader.ino
@@ -1,27 +1,9 @@
-/*
-* Du an: Doc nhiet do 2 kenh LM35 (A0, A1)
-* Mon: Mang Cam Bien - Tuan 03
-*/
-int adcValues[2]; 
-float nhietDo[2]; 
-char chuoi[30];
-
 void setup() {
   Serial.begin(9600);
 }
-
 void loop() {
-  // Doc kenh A0
-  adcValues[0] = analogRead(A0);
-  nhietDo[0] = (adcValues[0] * 500.0) / 1023.0;
-  
-  // Doc kenh A1
-  adcValues[1] = analogRead(A1);
-  nhietDo[1] = (adcValues[1] * 500.0) / 1023.0;
-  
-  // Dinh dang du lieu CSV: temp1,temp2
-  sprintf(chuoi, "%d,%d\n", (int)nhietDo[0], (int)nhietDo[1]);
-  Serial.print(chuoi);
-  
-  delay(100);
+  int val = analogRead(A0);
+  float temp = (val * 500.0) / 1023.0;
+  Serial.println(temp);
+  delay(500);
 }
---
+++ pc_app/TempMonitorApp/Program.cs
@@ -0,0 +1,6 @@
+using System;
+class Program {
+    static void Main() {
+        Console.WriteLine("Ung dung giam sat nhiet do LM35");
+    }
+}
